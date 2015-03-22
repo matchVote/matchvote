a14dc202d47c749605a8b7914a9762834aa04fc9
Brad Hankins <bradhankins@gmail.com>
2015-03-21 20:25:36 -0400

    Directory Page Styling

--- app/controllers/directory_controller.rb
+++ app/controllers/directory_controller.rb
@@ -1,5 +1,9 @@
 class DirectoryController < ApplicationController
   def index
     @reps = Representative.all
+    @sort_list = [
+      ["Alphabetically", "alphabetically"],
+      ["Similarity", "similarity"]
+    ]
   end
 end
