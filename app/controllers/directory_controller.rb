265ad4c394717f5c881de7f42f8e9d9cd33181a7
Brad Hankins <bradhankins@gmail.com>
2015-03-27 15:28:59 -0400

    Adding & Rearranging Items in Directory Sort List

--- app/controllers/directory_controller.rb
+++ app/controllers/directory_controller.rb
@@ -5,8 +5,8 @@ class DirectoryController < ApplicationController
       ["Sort by Name Recognition", "populatity"],
       ["Sort Alphabetically", "alphabetically"],
       ["Sort by Most Similar Views", "similarity"],
-      ["Sort by Approval Rating", "approval"],
       ["Sort by Least Similar Views", "difference"],
+      ["Sort by Approval Rating", "approval"],
       ["Sort by Seniority", "seniority"],
       ["Sort by Age", "age"],
       ["Sort by State", "state"]
