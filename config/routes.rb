02249d8149f7501a3bf4643df2531a2c22968ba1
bahelms <jimbonkgit@gmail.com>
2015-05-15 08:10:48 -0400

    adds custom user registrations controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: { registrations: "users/registrations" }
   root to: "directory#index"
   get "/representative/:slug", to: "representatives#show", as: "rep"
   resources :representatives, only: [:edit, :update]
