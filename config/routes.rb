ff9a7703aeeca1925bd002d5fca803fb09b68b5a
bahelms <jimbonkgit@gmail.com>
2015-05-29 09:00:07 -0400

    changes devise paths from /users to /citizens

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 Rails.application.routes.draw do
-  devise_for :users, controllers: { registrations: "users/registrations" }
+  devise_for :users, 
+    path: :citizens,
+    controllers: { registrations: "users/registrations" }
   root to: "directory#index"
+
   get "/representative/:slug", to: "representatives#show", as: "rep"
   resources :representatives, only: [:edit, :update]
   resources :citizens, only: [:show]
   resources :stances, only: [:index, :create, :update, :destroy]
-
   get "/directory/filter", to: "directory#filter"
 end
 
