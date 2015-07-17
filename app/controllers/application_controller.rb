a7cb854566e9f35603ccb7617bbcf6e51d6dd63d
bahelms <jimbonkgit@gmail.com>
2015-07-17 07:52:48 -0400

    makes navbar dropdown title dynamic

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,7 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   before_action :authenticate_user!
+  before_action :set_resource
 
   rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
 
@@ -11,5 +12,17 @@ class ApplicationController < ActionController::Base
     def user_not_authorized
       flash[:alert] = "You are not authorized to perform that action."
       redirect_to(request.referrer || root_url)
+    end
+
+    def set_resource
+      @resource = if navbar_dropdown_options.include?(params[:controller])
+        params[:controller]
+      else
+        "directory"
+      end
+    end
+
+    def navbar_dropdown_options
+      ["directory", "forum", "news", "elections", "townhall", "stances"]
     end
 end
